51ac34aa4933f553a589a6523b039512549027a3
Lan <lucy555xu@gmail.com>
2020-11-12 22:52:27 -0800

    add Feature1.cs and update Program.cs

---
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Feauture1
+{
+	public Feauture1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
